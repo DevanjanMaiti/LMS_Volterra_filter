a7278d18043cc3a127d9ac190111f220d5d8cc7f
Devanjan Maiti <devanjan008@gmail.com>
2014-08-14 15:17:29 +0530

    Create types.h

---
+++ types.h
@@ -0,0 +1,98 @@
+/*
+ * Voltera adaptive filter using LMS
+ * Definitions required by code.
+ *
+ * By
+ * Pranav S Koundinya
+ * Devanjan Maiti
+ * Sreekar V
+ * Nikhil Ravishankar
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <limits.h>
+
+/*=======================================================================*
+ * Target hardware information
+ *   Device type: 32-bit Generic
+ *   Number of bits:     char:   8    short:   16    int:  32
+ *                       long:  32      native word size:  32
+ *   Byte ordering: Unspecified
+ *   Signed integer division rounds to: Undefined
+ *   Shift right on a signed integer as arithmetic shift: on
+ *=======================================================================*/
+
+
+/*=======================================================================*
+ * Fixed width word size data types:                                     *
+ *   int8_T, int16_T, int32_T     - signed 8, 16, or 32 bit integers     *
+ *   uint8_T, uint16_T, uint32_T  - unsigned 8, 16, or 32 bit integers   *
+ *   real32_T, real64_T           - 32 and 64 bit floating point numbers *
+ *=======================================================================*/
+
+typedef signed char int8_T;
+typedef unsigned char uint8_T;
+typedef short int16_T;
+typedef unsigned short uint16_T;
+typedef int int32_T;
+typedef unsigned int uint32_T;
+typedef float real32_T;
+typedef double real64_T;
+
+/*===========================================================================*
+ * Generic type definitions: real_T, time_T, boolean_T, char_T, int_T,       *
+ *                           uint_T and byte_T.                              *
+ *===========================================================================*/
+
+typedef double real_T;
+typedef double time_T;
+typedef unsigned char boolean_T;
+typedef int int_T;
+typedef unsigned int uint_T;
+typedef char char_T;
+typedef char_T byte_T;
+
+/*=======================================================================*
+ * Min and Max:                                                          *
+ *   int8_T, int16_T, int32_T     - signed 8, 16, or 32 bit integers     *
+ *   uint8_T, uint16_T, uint32_T  - unsigned 8, 16, or 32 bit integers   *
+ *=======================================================================*/
+
+#define MAX_int8_T                      ((int8_T)(127))
+#define MIN_int8_T                      ((int8_T)(-128))
+#define MAX_uint8_T                     ((uint8_T)(255))
+#define MIN_uint8_T                     ((uint8_T)(0))
+#define MAX_int16_T                     ((int16_T)(32767))
+#define MIN_int16_T                     ((int16_T)(-32768))
+#define MAX_uint16_T                    ((uint16_T)(65535))
+#define MIN_uint16_T                    ((uint16_T)(0))
+#define MAX_int32_T                     ((int32_T)(2147483647))
+#define MIN_int32_T                     ((int32_T)(-2147483647-1))
+#define MAX_uint32_T                    ((uint32_T)(0xFFFFFFFFU))
+#define MIN_uint32_T                    ((uint32_T)(0))
+
+/* Logical type definitions */
+#if !defined(__cplusplus) && !defined(__true_false_are_keywords)
+# ifndef false
+# define false (0)
+# endif
+# ifndef true
+# define true (1)
+# endif
+#endif
+
+#ifndef TRUE
+# define TRUE (1)
+#endif
+#ifndef FALSE
+# define FALSE (0)
+#endif
+
+
+/* Block D-Work pointer type */
+typedef void * pointer_T;
+
+#endif                                  /* TYPES_H */
+
